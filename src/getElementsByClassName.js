3e6bfdad8893313b90ac69b5347ee8ca3be32ff7
Michael Flis <michaelflis07@gmail.com>
2017-12-13 15:41:25 -0800

    Complete Recursion get elements and JSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,39 @@
 //   return document.getElementsByClassName(className);
 // };
 
+/*You should use document.body, element.childNodes, and element.classList*/
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var output = [];
+
+  var getElements = function (element) {
+
+    // if node has a classList and
+    // if node clasList contains the class name
+    //   push node to the output
+    if ( element.classList && element.classList.contains(className) ) {
+      //debugger;
+      output.push(element);
+    }
+
+    // if node has child Nodes
+    //   check each child for the className
+    if ( element.childNodes ) {
+      for ( var i = 0; i < element.childNodes.length; i++ ) {
+        //console.log(i);
+        getElements(element.childNodes[i]);
+
+      }
+    }
+  };
+
+  //call to recursive function
+  getElements(document.body);
+
+  return output;
 };
+
+//var test = document.body.getElementsByClassName('targetClassName');
+//console.log(document.body.getElementsByClassName('targetClassName'));
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  var result = '';
+
+  if ( typeof obj === 'function' || obj === undefined) {
+    return '';
+  } else if ( obj === null ) {
+    result += '' + null;
+  } else if ( typeof obj === 'number' || typeof obj === 'boolean' ) {
+    result += '' + obj;
+  } else if ( typeof obj === 'string' ) {
+    result += '\"' + obj + '\"';
+  } else if ( Array.isArray(obj) ) {
+    result += '[';
+
+    if ( obj.length > 0 ) {
+      obj.forEach( (element, i) => {
+        if ( i === obj.length - 1 ) {
+          result += stringifyJSON(element);
+        } else {
+          result += stringifyJSON(element);
+          result += ',';
+        }
+      });
+    }
+
+    result += ']';
+  } else {
+    result += '{';
+    if ( Object.keys(obj).length ) {
+      Object.keys(obj).forEach( (keyName, j) => {
+        if ( typeof keyName !== 'function' && keyName !== undefined && typeof obj[keyName] !== 'function' && obj[keyName] !== undefined ) {
+          if ( j === Object.keys(obj).length - 1 ) {
+            result += stringifyJSON(keyName);
+            result += ':';
+            result += stringifyJSON(obj[keyName]);
+          } else {
+            result += stringifyJSON(keyName);
+            result += ':';
+            result += stringifyJSON(obj[keyName]);
+            result += ',';
+          }
+        }
+      });
+    }
+    result += '}';
+  }
+
+  return result;
 };
